2d4570b2ec458507ee2fe602a4f7dd66eb959f4f
Shilpa <parasianshilpa@gmail.com>
2019-09-17 14:23:44 -0400

    Added column names

--- newfile.py
+++ newfile.py
@@ -6,7 +6,15 @@ dtype=object
 ##pd.set_option('display.max_rows', 500)  
 ##pd.set_option('display.max_columns', 500)
 
-result = pd.read_csv('readfile.csv',low_memory=False)  
+result = pd.read_csv('readfile.csv',low_memory=False)
+
+##reading column headers
+text_file = pd.read_csv('column names.csv',sep='\t', header=None, low_memory=False) ##reading file with column names
+text_file.columns=['filename']
+column_name_arr = text_file.filename.get_values() ##converting column names into an array 
+
+##assigning column headers
+result.columns= column_name_arr
 
 print ("The number of row is " + str (result.shape[0])) 
 print ("\n  The number of column is "+ str (result.shape[1]))
